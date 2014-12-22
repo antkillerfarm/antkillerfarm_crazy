1422e9ce66153c66f0203bfb32b50ac93db86bff
antkillerfarm <antkillerfarm@gmail.com>
2014-12-23 00:07:28 +0800

    simple vfs is finished ^_^

--- linux_driver/simple-vfs/simple-vfs.c
+++ linux_driver/simple-vfs/simple-vfs.c
@@ -6,18 +6,38 @@
 MODULE_LICENSE("GPL");
 
 static struct proc_dir_entry *proc_entry;
+static int cnt = 0;
+static ssize_t proc_file_read(struct file *file, char __user *buf,
+		  size_t count, loff_t *ppos)
+{
+  printk(KERN_INFO "simple-vfs read:%d\n", cnt);
+  return 0;
+}
+
+static ssize_t proc_file_write(struct file *file, const char __user *buf,
+		   size_t count, loff_t *ppos)
+{
+  printk(KERN_INFO "simple-vfs write:%d\n", cnt);
+  cnt++;
+  return count;
+}
+
+static const struct file_operations proc_file_operations = {
+  .read  = proc_file_read,
+  .write  = proc_file_write,
+};
 
 /* Init function called on module entry */
 int my_module_init( void )
 {
-  proc_entry = create_proc_entry( "simple-vfs", 0644, NULL );
+  proc_entry = proc_create( "simple-vfs", 0666, NULL, &proc_file_operations);
   printk(KERN_INFO "my_module_init called.  Module is now loaded.\n");
   return 0;
 }
 /* Cleanup function called on module exit */
 void my_module_cleanup( void )
 {
-  remove_proc_entry("simple-vfs", &proc_root);
+  remove_proc_entry("simple-vfs", NULL);
   printk(KERN_INFO "my_module_cleanup called.  Module is now unloaded.\n");
   return;
 }
