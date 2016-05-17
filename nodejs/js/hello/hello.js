d1aa1bafd0640ea4ba349f148441fa86d125132f
antkillerfarm <antkillerfarm@gmail.com>
2016-05-17 23:44:21 +0800

    t

--- nodejs/js/hello/hello.js
+++ nodejs/js/hello/hello.js
@@ -1,1 +1,11 @@
-alert('Hello, world');
+var person = {
+    name: 'Bob',
+    age: 20,
+    tags: ['js', 'web', 'mobile'],
+    city: 'Beijing',
+    hasCar: true,
+    zipcode: null
+};
+
+//alert(person.name);
+aa.innerHTML = person.name + ":" + person.age;
