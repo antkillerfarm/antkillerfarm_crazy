bed318305f44d34f0c45a7b62b71d74bd8fc5653
antkillerfarm <antkillerfarm@gmail.com>
2016-05-17 19:51:28 +0800

    t

---
+++ gstreamer/muilti-room/gmediarender-2013-12-04/src/upnp_control_point_dev_ctrl.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <glib.h>
+
+#include <upnp/upnp.h>
+#include <upnp/ithread.h>
+#include <upnp/upnptools.h>
+
+#include "upnp_control_point.h"
+
