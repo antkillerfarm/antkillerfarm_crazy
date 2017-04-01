1b25c237a9a052ea7bf8029895c0f558908243ef
antkillerfarm <antkillerfarm@github.com>
2017-04-01 18:29:56 +0800

    t

---
+++ python/ml/nlp/jieba_demo.py
@@ -0,0 +1,34 @@
+import codecs
+import jieba
+
+#seg_list = jieba.cut("我来到北京清华大学", cut_all=False)
+#print("/".join(seg_list))
+
+fp1 = codecs.open("/home/tj/big_data/data/jinyong/utf8/1.txt", "r", "utf-8")
+fp2 = codecs.open("/home/tj/big_data/data/jinyong/utf8/c1.txt", "w", "utf-8")
+
+#line = "我来到北京清华大学".decode('utf-8')
+#seg_list = []
+#for i in range(0,len(line)):
+#    seg_list.append(line[i])
+    
+#print len(line)
+#print " ".join(seg_list)
+
+line =  fp1.readline()
+while line:
+    #seg_list = jieba.cut(line, cut_all=False)
+    seg_list = []
+    for i in range(0,len(line)):
+        seg_list.append(line[i])
+    
+    fp2.write(" ".join(seg_list))
+    #fp2.write(str(len(line)) +" ")
+    
+    line =  fp1.readline()
+
+fp2.close()
+fp1.close()
+
+#for i in range(0,len(line)):
+#    seg_list.append(line[i])
