3b363bfe4b213df509ab7c4f159d05af24231cad
antkillerfarm <antkillerfarm@gmail.com>
2017-04-16 23:33:37 +0800

    t

---
+++ python/ml/plot/super_plot2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(z):
+    # z*z*z is faster than z**3
+    return z*z*z - 1
+
+def df(z):
+    return 3*z*z
+
+def iterate(z):
+    num = 0
+    while abs(f(z)) > 1e-4:
+        w = z - f(z)/df(z)
+        num += np.exp(-1/abs(w-z))
+        z = w
+    return num
+
+
+def render(imgsize):
+    x = np.linspace(-1, 1, imgsize)
+    y = np.linspace(1, -1, imgsize)
+    z = x[None, :] + y[:, None] * 1j
+    img = np.frompyfunc(iterate, 1, 1)(z).astype(np.float)
+    fig = plt.figure(figsize=(imgsize/100.0, imgsize/100.0), dpi=100)
+    ax = fig.add_axes([0, 0, 1, 1], aspect=1)
+    ax.axis('off')
+    ax.imshow(img, cmap='hot')
+    fig.savefig('newton.png')
+
+
+if __name__ == '__main__':
+    import time
+    start = time.time()
+    render(imgsize=400)
+    end = time.time()
+    print('runtime: {:03f} seconds'.format(end - start))
--- python/ml/tensorflow/hello_cnn.py
+++ python/ml/tensorflow/hello_cnn.py
@@ -24,4 +24,14 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 W_conv1 = weight_variable([5, 5, 1, 32])
 b_conv1 = bias_variable([32])
 
+x_image = tf.reshape(x, [-1,28,28,1])
+
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
 
