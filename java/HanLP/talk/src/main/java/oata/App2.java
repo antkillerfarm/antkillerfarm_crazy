96f7479a1c3f5df5c41efa9002c56ee52bc165b9
antkillerfarm <antkillerfarm@gmail.com>
2017-05-10 10:42:55 +0800

    t

--- java/HanLP/talk/src/main/java/oata/App2.java
+++ java/HanLP/talk/src/main/java/oata/App2.java
@@ -22,18 +22,25 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+class Talk {
+    String from;
+    String to;
+    String talk;
+}
 public class App2 {
     public static void main(String[] args) {
+        //App.step1_2();
+        import_flowerplus_1();
         //App.step1();
         //step2();
         //step3();
         //step4();
-        //step5_1();
+        //step5_2_4();
         //step6_1();
         //step7();
         //test7();
         //step2_2();
-        test_dbscan();
+        //test_dbscan();
     }
     public static void step1() {
         List<String> corpus = FileReader.getAll("/home/tj/big_data/data/talk/2.txt", "txt");
@@ -216,6 +223,166 @@ public class App2 {
             e.printStackTrace();
         }
     }
+    public static void step5_2() {
+        try {
+            DataFrame<Object> df =  DataFrame.readCsv("/home/tj/big_data/data/talk/2j3sc_2.csv", ",",null,true);
+            DataFrame<Object> df2 = new DataFrame<>();
+            List<Nature> natureList = Arrays.asList(Nature.vn);
+            for (List<Object> row : df) {
+                Long start_num = (Long)row.get(0);
+                Long end_num = (Long)row.get(1);
+                String talk = (String)row.get(2);
+                String key = (String)row.get(3);
+                if (key == null) {
+                    continue;
+                }
+                List<Term> term_list = HanLP.segment(key);
+                String vn_key = "";
+                for (Term term : term_list) {
+                    if (natureList.contains(term.nature)) {
+                        vn_key += term.word + " ";
+                    }
+                }
+                df2.append(Arrays.asList(start_num, end_num, talk, key, vn_key));
+            }
+            df2.writeCsv("/home/tj/big_data/data/talk/2j3sc_4_1.csv");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void step5_2_2() {
+        try {
+            DataFrame<Object> df =  DataFrame.readCsv("/home/tj/big_data/data/talk/2j3sc_4_1.csv", ",",null,true);
+            DataFrame<Object> df2 = df.groupBy(4).count().sortBy(-1);
+            df2.writeCsv("/home/tj/big_data/data/talk/2j3sc_4_2.csv");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void step5_2_3() {
+        try {
+            DataFrame<Object> df =  DataFrame.readCsv("/home/tj/big_data/data/talk/2j3sc_4_1.csv", ",",null,true);
+            DataFrame<Object> df2 = DataFrame.readCsv("/home/tj/big_data/data/talk/2j3_6.csv", ",",null,true);
+            DataFrame<Object> df3 = new DataFrame<>();
+            DataFrame<Object> df5 = new DataFrame<>();
+            HashMap<String, String> talk_class = new HashMap<>();
+            int df_len = df.length();
+            int df2_len = df2.length();
+            for (int i = 0; i < df2_len - 1; i++) {
+                for (int j = i + 1; j < df2_len; j++) {
+                    String keyword0 = (String)df2.get(i,0);
+                    String keyword1 = (String)df2.get(j,0);
+                    List<String> kw_list = Arrays.asList(keyword0, keyword1);
+                    List<String> corpus =  new LinkedList<>();
+                    DataFrame<Object> df4 = new DataFrame<>();
+                    for (int k = 0; k < df_len; k++) {
+                        String vn_key = (String)df.get(k,4);
+                        if (vn_key == null) {
+                            continue;
+                        }
+                        int match_flag = 0;
+                        for (String kw : kw_list) {
+                            if (vn_key.contains(kw)) {
+                                match_flag++;
+                            }
+                        }
+                        if (match_flag == kw_list.size()) {
+                            Long start_num = (Long)df.get(k,0);
+                            Long end_num = (Long)df.get(k,1);
+                            String talk = (String)df.get(k,2);
+                            String talk_keyword = (String)df.get(k,3);
+                            String keywordStr = StringUtils.join(kw_list, " ");
+                            if (!talk_class.containsKey(String.valueOf(k))) {
+                                talk_class.put(String.valueOf(k),keywordStr);
+                                df4.append(Arrays.asList(start_num, end_num, talk, talk_keyword, vn_key, keywordStr));
+                                corpus.add(talk_keyword);
+                            }
+                        }
+                    }
+                    int clunum = 0;
+                    if (corpus.size() != 0) {
+                        DBScan ds = new DBScan();
+                        ArrayList<DBScan.DataObject> source = ds.formDataObject(corpus);
+                        clunum = ds.dbscan(source);
+                        int df4_len = df4.length();
+                        HashMap<String, String> cid_class = new HashMap<>();
+                        int source_num = 0;
+                        for (int n = 0; n < df4_len; n++) {
+                            int cid = source.get(n).getCid();
+                            boolean cid_flag = false;
+                            if (cid == -1) {
+                                cid_flag = true;
+                            } else
+                            {
+                                if (!cid_class.containsKey(String.valueOf(cid))) {
+                                    cid_class.put(String.valueOf(cid), "");
+                                    cid_flag = true;
+                                }
+                            }
+                            if (cid_flag) {
+                                Long start_num = (Long) df4.get(n, 0);
+                                Long end_num = (Long) df4.get(n, 1);
+                                String talk = (String) df4.get(n, 2);
+                                String talk_keyword = (String) df4.get(n, 3);
+                                String vn_key = (String) df4.get(n, 4);
+                                String keywordStr = (String) df4.get(n, 5);
+                                df3.append(Arrays.asList(start_num, end_num, talk, talk_keyword, vn_key, keywordStr, cid));
+                                source_num++;
+                            }
+                        }
+                        String keywordStr = (String) df4.get(0, 5);
+                        df5.append(Arrays.asList(keywordStr, source_num));
+                    }
+
+                    System.out.println(String.format("%d:%d:%d:%d", i, j, clunum, corpus.size()));
+                }
+            }
+
+            df3.writeCsv("/home/tj/big_data/data/talk/2j3sc_4_3.csv");
+            df5.writeCsv("/home/tj/big_data/data/talk/2j3sc_4_4.csv");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void step5_2_4() {
+        try {
+            DataFrame<Object> df =  DataFrame.readCsv("/home/tj/big_data/data/talk/2j3sc_4_3.csv", ",",null,true);
+            DataFrame<Object> df2 =  DataFrame.readCsv("/home/tj/big_data/data/talk/2j3sc_4_4.csv", ",",null,true);
+            FileReader.setFile_charset("UTF-8");
+            List<String> corpus = FileReader.getAll("/home/tj/big_data/data/talk/2jx.csv", "txt");
+            List<String> corpus2 = new LinkedList<>();
+            String cur_key = null;
+            String s = null;
+            for (List<Object> row : df) {
+                String keywordStr = (String)row.get(5);
+                if (!keywordStr.equals(cur_key)) {
+                    for (List<Object> row2 : df2) {
+                        String keywordStr2 = (String)row2.get(0);
+                        if (keywordStr2.equals(keywordStr)) {
+                            Long num = (Long)row2.get(1);
+                            s = "--------------------\n";
+                            s += String.format("%s:%d\n", keywordStr2, num);
+                            s += "--------------------\n";
+                            corpus2.add(s);
+                            break;
+                        }
+                    }
+                }
+                cur_key = keywordStr;
+                Long start_num = (Long)row.get(0);
+                Long end_num = (Long)row.get(1);
+                for (Long i = start_num - 1; i < end_num - 1; i++) {
+                    corpus2.add(corpus.get(i.intValue()));
+                }
+                s = "--------------------\n";
+                corpus2.add(s);
+            }
+            FileWriter.put("/home/tj/big_data/data/talk/2j3sc_4_5.txt",corpus2);
+            System.out.println("bye");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public static void step6() {
         try {
             DataFrame<Object> df =  DataFrame.readCsv("/home/tj/big_data/data/talk/2j3sc_4.csv", ",",null,true);
@@ -274,11 +441,32 @@ public class App2 {
             e.printStackTrace();
         }
     }
+    public static void step6_2() {
+        try {
+            DataFrame<Object> df =  DataFrame.readCsv("/home/tj/big_data/data/talk/2j3sc_6.csv", ",",null,true);
+            DataFrame<Object> df2 = new DataFrame<>("kw", "count", "cdf");
+            int num = 0;
+            for (List<Object> row : df) {
+                String talk_keyword = (String)row.get(0);
+                if (talk_keyword.contains("退款")) {
+                    continue;
+                }
+                Long talk_keyword_cnt = (Long)row.get(1);
+                if (talk_keyword.contains("vn")) {
+                    num += talk_keyword_cnt;
+                    df2.append(Arrays.asList(talk_keyword, talk_keyword_cnt, num));
+                }
+            }
+            df2.writeCsv("/home/tj/big_data/data/talk/2j3sc_6_2.csv");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public static void step7() {
         try {
-            DataFrame<Object> df =  DataFrame.readCsv("/home/tj/big_data/data/talk/2j3sc_4.csv", ",",null,true);
+            DataFrame<Object> df =  DataFrame.readCsv("/home/tj/big_data/data/talk/2j3sc_4_1.csv", ",",null,true);
             DataFrame<Object> df2 = new DataFrame<>();
-            String[] kw_list = {"退款","发货"};
+            String[] kw_list = {"邮发","活动"};
             for (List<Object> row : df) {
                 String talk_keyword = (String)row.get(3);
                 //String[] talk_kw_list = StringUtils.split(talk_keyword, " ");
@@ -292,7 +480,8 @@ public class App2 {
                     Long start_num = (Long)row.get(0);
                     Long end_num = (Long)row.get(1);
                     String talk = (String)row.get(2);
-                    df2.append(Arrays.asList(start_num, end_num, talk, talk_keyword));
+                    String vn_key = (String)row.get(4);
+                    df2.append(Arrays.asList(start_num, end_num, talk, talk_keyword, vn_key));
                 }
             }
 
@@ -460,9 +649,11 @@ public class App2 {
                 String talk_keyword = (String)row.get(3);
                 corpus.add(talk_keyword);
             }
+            System.out.println("step1");
             DBScan ds=new DBScan();
             ArrayList<DBScan.DataObject> source = ds.formDataObject(corpus);
             int clunum=ds.dbscan(source);
+            System.out.println("step2");
             int num = 0;
             for (DBScan.DataObject object : source) {
                 if (object.getCid() == -1) {
@@ -472,6 +663,76 @@ public class App2 {
             }
             System.out.println(String.format("%d:%d:%d", clunum, corpus.size(), num));
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean isIDValid(String role) {
+        if (role.startsWith("wx") || role.startsWith("TJ_IM_WEB_WX_USER_ACCOUNT_")) {
+            return true;
+        }
+        return false;
+    }
+    public static String formKey(String s1, String s2) {
+        String customer, service;
+        if (s1.startsWith("wx")) {
+            return s1+"/"+s2;
+        }
+        else {
+            return s2+"/"+s1;
+        }
+    }
+    public static void import_flowerplus() {
+        try {
+            DataFrame<Object> df =  DataFrame.readCsv("/home/tj/big_data/data/talk/3j.csv", ",",null,false);
+            DataFrame<Object> df2 = df.sortBy(1);
+            df2.writeCsv("/home/tj/big_data/data/talk/3j_2.csv");
+            System.out.println("bye");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void import_flowerplus_1() {
+        try {
+            DataFrame<Object> df =  DataFrame.readCsv("/home/tj/big_data/data/talk/3j.csv", ",",null,false);
+            Map<String,List<Talk>> talks_map = new HashMap<>();
+            List<String> corpus2 =  new LinkedList<>();
+            for (List<Object> row : df) {
+                String session_id = (String)row.get(1);
+                String from = (String)row.get(3);
+                String to = (String)row.get(5);
+                if (isIDValid(from) && isIDValid(to)) {
+                    String key = session_id;//formKey(from, to);
+                    List<Talk> talks;
+                    if (!talks_map.containsKey(key)) {
+                        talks = new LinkedList<>();
+                        talks_map.put(key,talks);
+                    } else {
+                        talks = talks_map.get(key);
+                    }
+                    Talk talk = new Talk();
+                    talk.from = from;
+                    talk.to = to;
+                    talk.talk = (String)row.get(6);
+                    talks.add(talk);
+                }
+            }
+            for (List<Talk> talks: talks_map.values()) {
+                for (Talk talk: talks) {
+                    String role;
+                    if (talk.from.startsWith("wx")) {
+                        role = "C";
+                    }else {
+                        role = "S";
+                    }
+                    String s = String.format("%s:%s", role, talk.talk);
+                    corpus2.add(s);
+                }
+                corpus2.add("----");
+            }
+            FileWriter.put("/home/tj/big_data/data/talk/3j_2.csv",corpus2);
+            System.out.println(String.format("%d", talks_map.size()));
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
