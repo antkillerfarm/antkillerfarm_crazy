b22679a71fe1a20a55ca35459034dd12c076af6f
antkillerfarm <antkillerfarm@gmail.com>
2017-08-09 21:35:35 +0800

    t

---
+++ python/misc/hello_argparse.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("x", type=int, help="the base")
+parser.add_argument("y", type=int, help="the exponent")
+parser.add_argument("-v", "--verbosity", action="count", default=0)
+args = parser.parse_args()
+
+
+
