7d606c9294a7b73f1ae538b09d71b64c636a8951
antkillerfarm <antkillerfarm@gmail.com>
2017-09-06 09:58:54 +0800

    add tikz example

--- python/ml/tensorflow/hello.py
+++ python/ml/tensorflow/hello.py
@@ -4,9 +4,10 @@
 import tensorflow as tf
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
-print sess.run(hello)
+print(sess.run(hello))
 
 a = tf.constant(10)
 b = tf.constant(32)
-print sess.run(a+b)
+print(sess.run(a+b))
+
 
--- python/ml/tensorflow/hello_cnn.py
+++ python/ml/tensorflow/hello_cnn.py
@@ -60,10 +60,10 @@ for i in range(20000):
     if i%100 == 0:
         train_accuracy = accuracy.eval(feed_dict={
             x:batch[0], y_: batch[1], keep_prob: 1.0})
-        print "step %d, training accuracy %g"%(i, train_accuracy)
+        print("step %d, training accuracy %g" %(i, train_accuracy))
     train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-print "test accuracy %g"%accuracy.eval(feed_dict={
-    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
+print("test accuracy %g"%accuracy.eval(feed_dict={
+    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
 
