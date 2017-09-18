ac541c48d34e5185ad244d30cb2db8ec5aab18cd
antkillerfarm <antkillerfarm@gmail.com>
2017-09-18 21:12:39 +0800

    t

--- python/ml/hello/decision_tree.py
+++ python/ml/hello/decision_tree.py
@@ -5,15 +5,21 @@ import numpy as np
 from sklearn import datasets
 from sklearn import tree
 import graphviz
+import xgboost as xgb
+import matplotlib.axes as axes
 
 iris = datasets.load_iris()
 X = iris.data
 y = iris.target
 
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(X, y)
+#clf = tree.DecisionTreeClassifier()
+#clf = clf.fit(X, y)
 
-dot_data = tree.export_graphviz(clf, out_file=None)
-graph = graphviz.Source(dot_data)
-graph.render("iris")
+#dot_data = tree.export_graphviz(clf, out_file=None)
+#graph = graphviz.Source(dot_data)
+#graph.render("iris")
+
+clf = xgb.XGBClassifier()
+clf = clf.fit(X, y)
+xgb.plot_tree(clf)
 
