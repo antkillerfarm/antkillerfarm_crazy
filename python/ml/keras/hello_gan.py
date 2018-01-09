6588fe80be5cf7d3e3c3b5dfcdd9eb546fd387e2
antkillerfarm <antkillerfarm@gmail.com>
2018-01-09 14:06:14 +0800

    t

--- python/ml/keras/hello_gan.py
+++ python/ml/keras/hello_gan.py
@@ -23,7 +23,7 @@ RND = 777
 # output settings
 RUN = 'B'
 OUT_DIR = 'out/' + RUN
-TENSORBOARD_DIR = '/tensorboard/wgans/' + RUN
+TENSORBOARD_DIR = 'tensorboard/wgans/' + RUN
 SAVE_SAMPLE_IMAGES = False
 
 # GPU # to run on
@@ -140,6 +140,58 @@ def create_G(Z_SIZE=Z_SIZE):
 
     return Model(inputs=[input_z, input_class], outputs=x, name='G')
 
+# save 10x10 sample of generated images
+def generate_samples(n=0, save=True):
+
+    zz = np.random.normal(0., 1., (100, Z_SIZE))
+    generated_classes = np.array(list(range(0, 10)) * 10)
+    generated_images = G.predict([zz, generated_classes.reshape(-1, 1)])
+
+    rr = []
+    for c in range(10):
+        rr.append(
+            np.concatenate(generated_images[c * 10:(1 + c) * 10]).reshape(
+                280, 28))
+    img = np.hstack(rr)
+
+    if save:
+        plt.imsave(OUT_DIR + '/samples_%07d.png' % n, img, cmap=plt.cm.gray)
+
+    return img
+
+def update_tb_summary(step, write_sample_images=True):
+
+    s = tf.Summary()
+
+    # losses as is
+    for names, vals in zip((('D_real_is_fake', 'D_real_class'),
+                            ('D_fake_is_fake', 'D_fake_class'), ('DG_is_fake',
+                                                                 'DG_class')),
+                           (D_true_losses, D_fake_losses, DG_losses)):
+
+        v = s.value.add()
+        v.simple_value = vals[-1][1]
+        v.tag = names[0]
+
+        v = s.value.add()
+        v.simple_value = vals[-1][2]
+        v.tag = names[1]
+
+    # D loss: -1*D_true_is_fake - D_fake_is_fake
+    v = s.value.add()
+    v.simple_value = -D_true_losses[-1][1] - D_fake_losses[-1][1]
+    v.tag = 'D loss (-1*D_real_is_fake - D_fake_is_fake)'
+
+    # generated image
+    if write_sample_images:
+        img = generate_samples(step, save=True)
+        s.MergeFromString(tf.Session().run(
+            tf.summary.image('samples_%07d' % step,
+                             img.reshape([1, *img.shape, 1]))))
+
+    sw.add_summary(s, step)
+    sw.flush()
+
 # create output dir
 if not os.path.isdir(OUT_DIR): os.makedirs(OUT_DIR)
 
@@ -183,4 +235,92 @@ y_train = np.concatenate((y_train, y_test))
 # convert to -1..1 range, reshape to (sample_i, 28, 28, 1)
 X_train = (X_train.astype(np.float32) - 127.5) / 127.5
 X_train = np.expand_dims(X_train, axis=3)
+
+# write tensorboard summaries
+sw = tf.summary.FileWriter(TENSORBOARD_DIR)
+
+progress_bar = Progbar(target=ITERATIONS)
+
+DG_losses = []
+D_true_losses = []
+D_fake_losses = []
+
+for it in range(ITERATIONS):
+
+    if len(D_true_losses) > 0:
+        progress_bar.update(
+            it,
+            values=[  # avg of 5 most recent
+                ('D_real_is_fake', np.mean(D_true_losses[-5:], axis=0)[1]),
+                ('D_real_class', np.mean(D_true_losses[-5:], axis=0)[2]),
+                ('D_fake_is_fake', np.mean(D_fake_losses[-5:], axis=0)[1]),
+                ('D_fake_class', np.mean(D_fake_losses[-5:], axis=0)[2]),
+                ('D(G)_is_fake', np.mean(DG_losses[-5:], axis=0)[1]),
+                ('D(G)_class', np.mean(DG_losses[-5:], axis=0)[2])
+            ]
+        )
+
+    else:
+        progress_bar.update(it)
+
+    # 1: train D on real+generated images
+
+    if (it % 1000) < 25 or it % 500 == 0:  # 25 times in 1000, every 500th
+        d_iters = 100
+    else:
+        d_iters = D_ITERS
+
+    for d_it in range(d_iters):
+
+        # unfreeze D
+        D.trainable = True
+        for l in D.layers: l.trainable = True
+
+        # clip D weights
+
+        for l in D.layers:
+            weights = l.get_weights()
+            weights = [np.clip(w, -0.01, 0.01) for w in weights]
+            l.set_weights(weights)
+
+        # 1.1: maximize D output on reals === minimize -1*(D(real))
+
+        # draw random samples from real images
+        index = np.random.choice(len(X_train), BATCH_SIZE, replace=False)
+        real_images = X_train[index]
+        real_images_classes = y_train[index]
+
+        D_loss = D.train_on_batch(real_images, [-np.ones(BATCH_SIZE),
+                                                real_images_classes])
+        D_true_losses.append(D_loss)
+
+        # 1.2: minimize D output on fakes
+
+        zz = np.random.normal(0., 1., (BATCH_SIZE, Z_SIZE))
+        generated_classes = np.random.randint(0, 10, BATCH_SIZE)
+        generated_images = G.predict([zz, generated_classes.reshape(-1, 1)])
+
+        D_loss = D.train_on_batch(generated_images, [np.ones(BATCH_SIZE),
+                                                     generated_classes])
+        D_fake_losses.append(D_loss)
+
+    # 2: train D(G) (D is frozen)
+    # minimize D output while supplying it with fakes,
+    # telling it that they are reals (-1)
+
+    # freeze D
+    D.trainable = False
+    for l in D.layers: l.trainable = False
+
+    zz = np.random.normal(0., 1., (BATCH_SIZE, Z_SIZE))
+    generated_classes = np.random.randint(0, 10, BATCH_SIZE)
+
+    DG_loss = DG.train_on_batch(
+        [zz, generated_classes.reshape((-1, 1))],
+        [-np.ones(BATCH_SIZE), generated_classes])
+
+    DG_losses.append(DG_loss)
+
+    if it % 10 == 0:
+        update_tb_summary(it, write_sample_images=(it % 250 == 0))
 
