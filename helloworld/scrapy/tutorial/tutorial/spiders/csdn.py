a463097d370a1a6d7568499ecf26fa3c4af8fc14
antkillerfarm <antkillerfarm@sohu.com>
2018-05-06 21:51:48 +0800

    t

--- helloworld/scrapy/tutorial/tutorial/spiders/csdn.py
+++ helloworld/scrapy/tutorial/tutorial/spiders/csdn.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import string
+import re
 from tutorial.items import TutorialItem
 
 class CsdnSpider(scrapy.Spider):
     name = "csdn"
     allowed_domains = ["csdn.net"]
     start_urls = (
-        'http://blog.csdn.net/antkillerfarm/svc/getarticles?pageindex=1&pagesize=100',)
+        'https://blog.csdn.net/antkillerfarm/article/list/1','https://blog.csdn.net/antkillerfarm/article/list/2',
+        'https://blog.csdn.net/antkillerfarm/article/list/3','https://blog.csdn.net/antkillerfarm/article/list/4',
+        'https://blog.csdn.net/antkillerfarm/article/list/5','https://blog.csdn.net/antkillerfarm/article/list/6')
 
     def parse(self, response):
         sel = scrapy.Selector(response)
-        sites = sel.xpath('//li[@class="blog-unit"]')
+        sites = sel.xpath('//div[@class="article-item-box csdn-tracking-statistics"]')
         items = []
         num = 0
 
         for site in sites:
             item = TutorialItem()
-            item['title'] = site.xpath('descendant::h3[@class="blog-title odd-overhidden bottom-dis-8"]/a/text()').extract()[0].encode("utf-8").strip()
-            item['readCount'] = site.xpath('descendant::div[@class=" floatL left-dis-24"]/span/text()').extract()[0].strip()
+            item['title'] = site.xpath('descendant::h4/a/text()').extract()[1].encode("utf-8").strip()
+            item['readCount'] = site.xpath('descendant::span[@class="read-num"]/text()').extract()[0].strip().replace("阅读数：".encode("utf-8"),"")
             items.append(item)
             print "title:="+item['title']
             print "readCount:="+item['readCount']
