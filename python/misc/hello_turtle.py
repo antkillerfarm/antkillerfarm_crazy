2e05ffd3e054fe5aca2297a86d87bdfd9054a095
antkillerfarm <antkillerfarm@gmail.com>
2019-01-22 16:46:34 +0800

    t

---
+++ python/misc/hello_turtle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from turtle import *
+color('red', 'yellow')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
