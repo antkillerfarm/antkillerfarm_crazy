26ae588a1c49c7bcc8f8c338da496cb8c27ff618
antkillerfarm <antkillerfarm@sohu.com>
2021-11-26 17:27:42 +0800

    t

--- python/ml/pytorch/pytorch_hook.py
+++ python/ml/pytorch/pytorch_hook.py
@@ -10,8 +10,8 @@ def hook_forward_fn(module, input, output):
 # model = torchvision.models.quantization.mobilenet_v2(pretrained=True, quantize=True)
 model = torchvision.models.mobilenet_v2(pretrained=True)
 dummy_input = torch.rand(1, 3, 224, 224)
-summary(model, input_size=(1, 3, 224, 224))
-print(model)
+# summary(model, input_size=(1, 3, 224, 224))
+# print(model)
 layer_name = 'features.1.conv.0.0'
 for (name, module) in model.named_modules():
     # print("Layer Name: " + name)
@@ -27,9 +27,18 @@ l = list(children)
 model_1 = l[0]
 children = model_1.children()
 model_1 = list(children)[0]
-print(model_1)
+# print(model_1)
 
 output = model_1(dummy_input)
 # print(output)
 
+model = torchvision.models.quantization.mobilenet_v2(pretrained=True, quantize=True)
+print(model)
+children = model.children()
+l = list(children)
+children = l[0].children()
+l_cut_model = [l[2], list(children)[0], l[3]]
+model_1 = torch.nn.Sequential(*l_cut_model)
+print(model_1)
+output = model_1(dummy_input)
 print("OK")
