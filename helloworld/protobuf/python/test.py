03644f84cbc79c53a87d8f1c2b291b7da3acec58
antkillerfarm <antkillerfarm@sohu.com>
2022-04-11 23:06:53 +0800

    t

--- helloworld/protobuf/python/test.py
+++ helloworld/protobuf/python/test.py
@@ -1,16 +1,14 @@
-import google.protobuf
 from test_pb2 import TestMsg
 import time
 
 test = TestMsg()
 test.id=1
 test.time=int(time.time())
-test.string="asdftest"
-print test
+test.note="asdftest"
+print(test)
 test_str = test.SerializeToString()
-print test_str
+print(test_str)
 
 test1 = TestMsg()
 test1.ParseFromString(test_str)
-print test1
-
+print(test1)
