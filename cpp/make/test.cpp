1a3f5f7d9453466a239901e1404a2a010cd70d95
antkillerfarm <antkillerfarm@gmail.com>
2023-01-10 10:25:11 +0800

    t

--- cpp/make/test.cpp
+++ cpp/make/test.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <vector>
+
+std::vector<int> f1() {
+    std::vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    return v;
+}
+
+void f2(std::vector<int>& v) {
+    auto v1 = f1();
+    v = std::move(v1);
+    std::cout << "test0 : " << v.size() << std::endl;
+}
 
 int main()
 {
-    std::cout << "test" << std::endl;
+    std::vector<int> v;
+    f2(v);
+    std::cout << "test1 : " << v.size() << std::endl;
     return 0;
 }
